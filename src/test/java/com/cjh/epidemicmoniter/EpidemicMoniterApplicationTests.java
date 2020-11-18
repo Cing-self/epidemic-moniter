179b26e3baefce8ffda1eeaa647f23bb5b98d0c0
overclound <353674054@qq.com>
2020-11-18 10:06:47 +0800

    Initial commit

---
+++ src/test/java/com/cjh/epidemicmoniter/EpidemicMoniterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjh.epidemicmoniter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpidemicMoniterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
